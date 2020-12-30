be6a82aab70397441ea0cff3495262916eeec182
alanflp <alanflpns@gmail.com>
2020-12-30 15:45:09 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Axios from 'axios'
+// const USUARIOS = [
+//   {
+//     id: 1,
+//     nome: 'teste1'
+//   },
+//   {
+//     id: 2,
+//     nome: 'teste2'
+//   },
+//   {
+//     id: 3,
+//     nome: 'teste3'
+//   },
+//   {
+//     id: 4,
+//     nome: 'teste4'
+//   },
+//   {
+//     id: 5,
+//     nome: 'teste5'
+//   }
+// ]
 
 function App() {
+  const [usuarios, setUsuarios] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    getUsuarios()
+  }, [])
+
+  // function getUsuarios() {
+  //   setLoading(true)
+
+  //   setTimeout(() => {
+  //     setUsuarios(USUARIOS)
+  //     setLoading(false)
+  //   }, 1000)
+  // }
+
+  async function getUsuarios() {
+    setLoading(true)
+
+    const response = (await Axios.get('http://localhost:3333/usuarios')).data
+
+    setUsuarios(response)
+    setLoading(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table border='1' >
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+          </tr>
+        </thead>
+        <tbody>
+          {loading ?
+            <span>carregando...</span>
+            :
+            usuarios.map(usuario => (
+              <tr key={usuario.id}>
+                <td>{usuario.id}</td>
+                <td>{usuario.nome}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
